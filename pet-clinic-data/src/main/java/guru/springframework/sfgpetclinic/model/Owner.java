9b262f36b62e49cca738defad93fa2b379665d3b
Asike <sapasask@gmail.com>
2022-02-06 22:48:13 +0600

    Updated owner with JPA entity. Closes #36

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,21 +1,29 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Owner  extends Person{
+@Entity
+@Table(name = "owners")
+public class Owner  extends Person {
 
-    private String Address;
+    @Column(name = "address")
+    private String address;
+    @Column(name = "city")
     private String city;
+    @Column(name = "telephone")
     private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
-        return Address;
+        return address;
     }
 
     public void setAddress(String address) {
-        Address = address;
+        address = address;
     }
 
     public String getCity() {
